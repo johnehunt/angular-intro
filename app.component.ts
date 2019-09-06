036961b6e73a29989980f60e50d4b9ecd2e78954
John Hunt <john@matraining.com>
2019-09-06 13:27:25 +0100

    Removed files in wrong location

--- app.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-root',
-  template: `<p changecolor>{{message}}</p>`
-})
-export class AppComponent {
-  message = 'Hello!';
-}
